66b195741cc65bc2131ca316e685ca9d64eb8967
Cleanmorty56 <dimalakaev@gmail.com>
2024-02-01 17:59:21 +0500

    zaputalsya

--- 2.py
+++ 2.py
@@ -4,7 +4,3 @@ def sobsstevnayapohvala(name):
     print(f"Я самый лучший человек в мире, ведь я {name}!")
 sobsstevnayapohvala(name)
 
-name1 = "Кое-кто"
-def nepravda(name1):
-    print(f"Но {name1} говорит, что это не так...")
-nepravda(name1)
